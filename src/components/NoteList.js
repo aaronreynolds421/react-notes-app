9b635809fe6785053c5bbfbc79482ba020c5d971
Aaron Reynolds <94026105+aaronreynolds421@users.noreply.github.com>
2025-04-06 09:59:39 -0700

    making leadway

---
+++ src/components/NoteList.js
@@ -0,0 +1,13 @@
+import Notes from './Notes';
+
+const NoteList = () => {
+  return (
+    <div className="notes-list">
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+    </div>
+  );
+};
+export default NoteList;
